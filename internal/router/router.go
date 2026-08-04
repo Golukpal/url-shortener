1790090436ee245bfeb630c50914a12d89041ff7
Golu Kumar Pal <golukpal@ssipmt.com>
2026-08-05 01:22:10 +0530

    add router

---
+++ internal/router/router.go
@@ -0,0 +1,24 @@
+package router
+
+import (
+	"net/http"
+
+	"url-shortener/internal/app"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Setup(a *app.App) *gin.Engine {
+
+	r := gin.New()
+
+	r.GET("/health", func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+
+	})
+
+	return r
+}
